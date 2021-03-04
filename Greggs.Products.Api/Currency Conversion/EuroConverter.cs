0bed784e671e942640f8854e76fb58fca702aadb
Peter K <peter.kaczorek@icloud.com>
2021-03-04 10:21:55 +0000

    implemented currency conversion
    implemented currency conversion

---
+++ Greggs.Products.Api/Currency Conversion/EuroConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Greggs.Products.Api.Currency
+{
+    public class EuroConverter : ICurrencyConverter
+    {
+        private const decimal exchangeRate = 1.11m;
+
+        //Convert to Euro based on the constant exchange rate
+        public decimal Convert(decimal price)
+        {
+            return Round(price * exchangeRate);
+        }
+
+        private decimal Round(decimal price)
+        {
+            return Math.Round(price, 2);
+        }
+    }
+}
---
+++ Greggs.Products.Api/Currency Conversion/ICurrencyConverter.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Greggs.Products.Api.Currency
+{
+    public interface ICurrencyConverter
+    {
+        decimal Convert(decimal price);
+    }
+}
